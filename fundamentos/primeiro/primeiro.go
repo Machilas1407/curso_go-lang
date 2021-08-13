f301efed8d77732c908cdb65a08c5dc2974ac51b
machilas <marcello.machilas@gmail.com>
2021-08-13 17:37:37 -0300

    Primeiros projetos do curso

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,23 @@
+// Programa executáveid iniciam pelio pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A porta de entrada de um programa Go é a função main
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
+
+/*
+	Sobre os comentários
+
+	1) Priorize código leggível e faça comentátios que agrega valor!
+	2) Evite comtários óbvios
+	3) Durente o curso abse dos comentários
+*/
